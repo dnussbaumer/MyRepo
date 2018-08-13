d5a66f231b5a34aaea56f093e09ba7bf798862c0
Dan Nussbaumer <dnussbaumer@reedgroup.com>
2018-08-13 14:40:09 -0600

    breaking out library files

---
+++ LeavePro.AT.Pipeline/vars/addChanges.groovy
@@ -0,0 +1,5 @@
+String call(String changeLines, String optionLine) {
+    changeLines += ".\\AutoCS.ps1 -ConfigFile .\\ChangeScript.config -Environment AT -Schema ${optionLine} -SkipBuildDb -SkipCompare -SkipTFSChangeScript\n"
+    changeLines += ".\\AutoCS.ps1 -ConfigFile .\\ChangeScript.config -Environment AT -Schema ${optionLine} -SkipCompare -SkipTFSChangeScript -SkipCreateFiles\n"
+    changeLines += ".\\AutoCS.ps1 -ConfigFile .\\ChangeScript.config -Environment AT -Schema ${optionLine} -SkipCreateFiles -SkipBuildDb \n"
+}
