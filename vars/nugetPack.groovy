11b9c581d30b1d9d107ed31a9ed7ace7c7f2dd1b
Dan Nussbaumer <dnussbaumer@reedgroup.com>
2018-08-15 15:58:00 -0600

    update

--- vars/nugetPack.groovy
+++ vars/nugetPack.groovy
@@ -1,7 +1,7 @@
-def call(workSpace, version, packageLocation) {
-    bat 'if exist artifacts rmdir artifacts /s /q'
-    bat 'mkdir artifacts'
-    bat "\"C:\\Program Files (x86)\\Nuget\\nuget.exe\" pack \"${workSpace}\\ccfDir\\UAT\\AT_CCF.nuspec\" -Version ${version} -OutputDirectory ccfDir/artifacts/ -NoPackageAnalysis"
-    bat "\"C:\\Program Files (x86)\\Nuget\\nuget.exe\" push -source ${packageLocation}/Platform ccfDir/artifacts/AT_CCF.${version}.nupkg"
+def call(nuspecLocations, version) {
+    def endingIndex = ${nuspecLocations}.size() - 1
+    for (i in 0..endingIndex) {
+        bat "\"C:\\Program Files (x86)\\Nuget\\nuget.exe\" pack \"${nuspecLocations[i]}\" -Version ${version} -OutputDirectory ccfDir/artifacts/ -NoPackageAnalysis"
+    }
 }
 
