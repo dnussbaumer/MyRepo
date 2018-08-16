fa7a97534d70d15c4e233510e28c580f597403d6
Dan Nussbaumer <dnussbaumer@reedgroup.com>
2018-08-16 08:59:43 -0600

    update

--- vars/nugetPack.groovy
+++ vars/nugetPack.groovy
@@ -1,10 +1,7 @@
 def call(nuspecLocations, version) {
-    echo 'Inside method'
     def endingIndex = nuspecLocations.size() - 1
     for (i in 0..endingIndex) {
-        echo "nuspecLocations = ${nuspecLocations[i]}"
-        echo "version = ${version}"
-//        bat "\"C:\\Program Files (x86)\\Nuget\\nuget.exe\" pack \"${nuspecLocations[i]}\" -Version ${version} -OutputDirectory ccfDir/artifacts/ -NoPackageAnalysis"
+        bat "\"C:\\Program Files (x86)\\Nuget\\nuget.exe\" pack \"${nuspecLocations[i]}\" -Version ${version} -OutputDirectory artifacts/ -NoPackageAnalysis"
     }
 }
 
