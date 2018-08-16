823170b14be7361741e78263398f8a47d1232bdc
Dan Nussbaumer <dnussbaumer@reedgroup.com>
2018-08-16 10:37:53 -0600

    update

--- vars/nugetPack.groovy
+++ vars/nugetPack.groovy
@@ -1,11 +1,11 @@
 def call(nuspecLocations, version) {
     def batCommand = ""
     def endingIndex = nuspecLocations.size() - 1
+    bat 'if exist artifacts rmdir artifacts /s /q'
+    bat 'mkdir artifacts'
     for (i in 0..endingIndex) {
         batCommand += "\"C:\\Program Files (x86)\\Nuget\\nuget.exe\" pack \"${nuspecLocations[i]}\" -Version ${version} -OutputDirectory artifacts/ -NoPackageAnalysis\n"
-//        bat "\"C:\\Program Files (x86)\\Nuget\\nuget.exe\" pack \"${nuspecLocations[i]}\" -Version ${version} -OutputDirectory artifacts/ -NoPackageAnalysis"
     }
     bat batCommand
-//    return batCommand
 }
 
