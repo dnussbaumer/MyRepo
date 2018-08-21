034452c72186e13cf1f0d554057baff9f31be7d9
Dan Nussbaumer <dnussbaumer@reedgroup.com>
2018-08-21 15:25:54 -0600

    update

--- vars/findDbChanges.groovy
+++ vars/findDbChanges.groovy
@@ -1,25 +1,16 @@
-def call(List configList, file) {
-    def endingOuterIndex = configList.size() - 1
+def call(project, configFileText, file) {
+    def configLines = configFileText.split("\n")
+    def endingOuterIndex = configLines.size() - 1
     def endingInnerIndex = 0
     def changeFound = 'false'
     def optionLine = ''
     for (i in 0..endingOuterIndex) {
-        if (changeFound == 'false') {
-            changeFound = configList[i][1]
-        }
-        endingInnerIndex = configList[i][3].size() -1
-        for (j in 0..endingInnerIndex) {
-            if (file.path.contains(configList[i][3][j])) {
+        def configValues = configLines[i].split(',')
+            if (configValues[0] == project && file.path.contains(configValues[2])) {
                 changeFound = 'true'
-                if (!optionLine.contains(configList[i][0])) {
-                    optionLine = configList[i][0] + ' '
-                }
-                if (!optionLine.contains(configList[i][2])) {
-                    optionLine += configList[i][2]
-                }
+                optionLine += configValues[1]
             }
-        }
     }
-    List returnList = [changeFound,optionLine]    
+    def returnList = [changeFound,optionLine]    
     return returnList
 }
