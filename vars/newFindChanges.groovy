3856ecb4382fa55dc7375a1c043f58f477605713
Dan Nussbaumer <dnussbaumer@reedgroup.com>
2018-08-21 11:21:54 -0600

    update

--- vars/newFindChanges.groovy
+++ vars/newFindChanges.groovy
@@ -1,13 +1,13 @@
 def call(project, configFilePath, file) {
     echo 'Entering method call'
     echo "Attempting to read file ${configFilePath}"
-//    def configText = readFile(configFilePath)
-    def configText = "CCF,\$/Support/Central Configuration Files/BAT\n" +
-                    "CCF,\$/Support/Central Configuration Files/SIT\n" +
-                    "CCF,\$/Support/Central Configuration Files/Stage/Reed Services\n" +
-                    "CCF,\$/Support/Central Configuration Files/Stage/SaaS\n" +
-                    "CCF,\$/Support/Central Configuration Files/Stage/Canada\n" +
-                    "CCF,\$/Support/Central Configuration Files/UAT/Curly"
+    def configText = readFile(configFilePath)
+//    def configText = "CCF,\$/Support/Central Configuration Files/BAT\n" +
+//                    "CCF,\$/Support/Central Configuration Files/SIT\n" +
+//                    "CCF,\$/Support/Central Configuration Files/Stage/Reed Services\n" +
+//                    "CCF,\$/Support/Central Configuration Files/Stage/SaaS\n" +
+//                    "CCF,\$/Support/Central Configuration Files/Stage/Canada\n" +
+//                    "CCF,\$/Support/Central Configuration Files/UAT/Curly"
     echo 'file read'
     def configLines = configText.split('\n')
     echo 'lines split'
