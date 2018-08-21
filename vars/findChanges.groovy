2734b64104d4707a0b3ecc18d841761036985f6d
Dan Nussbaumer <dnussbaumer@reedgroup.com>
2018-08-21 13:48:45 -0600

    update

--- vars/findChanges.groovy
+++ vars/findChanges.groovy
@@ -1,19 +1,14 @@
-def call(List configList, file) {
-    def endingOuterIndex = configList.size() - 1
-    def endingInnerIndex = 0
+def call(project, configFileText, file) {
+    def configLines = configFileText.split('\n')
     def changeFound = 'false'
-    def i = 0
-    while (i <= endingOuterIndex && changeFound == 'false') {
-        changeFound = configList[i][1]
-        if (changeFound == 'false') {
-            endingInnerIndex = configList[i][2].size() -1
-            for (j in 0..endingInnerIndex) {
-                if (file.path.contains(configList[i][2][j])) {
-                    changeFound = 'true'
-                }
-            }
+
+    def configEndingIndex = configLines.size() - 1
+    for (i in 0..configEndingIndex) {
+        def configValues = configLines[i].split(',')
+        if (configValues[0] == project && file.path.contains(configValues[1])) {
+            echo "found change in ${project}"
+            changeFound = 'true'
         }
-        i++
     }
     return changeFound
 }
