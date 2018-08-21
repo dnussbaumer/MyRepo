843babe35f96a822cd40a183317cf7f33671b171
Dan Nussbaumer <dnussbaumer@reedgroup.com>
2018-08-21 13:10:16 -0600

    update

--- vars/newFindChanges.groovy
+++ vars/newFindChanges.groovy
@@ -1,30 +1,14 @@
-@NonCPS
 def call(project, configFileText, file) {
-    echo 'Entering method call'
-    echo "project = ${project}"
-    echo "file = ${file.path.toString()}"
-    echo 'configFileText:'
-    echo configFileText
-//    def configText = "CCF,\$/Support/Central Configuration Files/BAT\n" +
-//                    "CCF,\$/Support/Central Configuration Files/SIT\n" +
-//                    "CCF,\$/Support/Central Configuration Files/Stage/Reed Services\n" +
-//                    "CCF,\$/Support/Central Configuration Files/Stage/SaaS\n" +
-//                    "CCF,\$/Support/Central Configuration Files/Stage/Canada\n" +
-//                    "CCF,\$/Support/Central Configuration Files/UAT/Curly"
-//    echo 'file read'
-//    def configLines = configText.split('\n')
-//    echo 'lines split'
+    def configLines = configFileText.split('\n')
     def changeFound = 'false'
-//    
-//    def configEndingIndex = configLines.size() - 1
-//    for (i in 0..configEndingIndex) {
-//        def configValues = configLines[i].split(',')
-//        echo 'values split'
-//        if (configValues[0] == project && file.path.contains(configValues[1])) {
-//            echo "found change in ${project}"
-//            changeFound = 'true'
-//        }
-//    }
-    echo 'returning'
+
+    def configEndingIndex = configLines.size() - 1
+    for (i in 0..configEndingIndex) {
+        def configValues = configLines[i].split(',')
+        if (configValues[0] == project && file.path.contains(configValues[1])) {
+            echo "found change in ${project}"
+            changeFound = 'true'
+        }
+    }
     return changeFound
 }
