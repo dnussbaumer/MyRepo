cb42263bbba02667dc29e85bb751b390211b5047
Dan Nussbaumer <dnussbaumer@reedgroup.com>
2018-08-22 08:55:21 -0600

    update

--- vars/findChanges.groovy
+++ vars/findChanges.groovy
@@ -10,7 +10,13 @@ def call(project, configFileText, file) {
         echo "configValue project = ${configValues[0]}"
         echo "file path = ${file.path.toString()}"
         echo "configValues path = ${configValues[1]}"
-        if (configValues[0] == project && file.path.contains(configValues[1])) {
+        if (configValues[0] == project) {
+            echo 'Correct Project'
+        }
+        if (file.path.contains(configValues[1])) {
+            echo 'Correct Path'
+        }
+        if ((configValues[0] == project) && (file.path.contains(configValues[1]))) {
             echo "found change in ${project}"
             changeFound = 'true'
         }
