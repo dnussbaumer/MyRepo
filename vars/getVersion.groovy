b149a16174ac2bd2990e2a207be2fcc8ef1eae68
Dan Nussbaumer <dnussbaumer@reedgroup.com>
2018-08-23 16:28:46 -0600

    update

---
+++ vars/getVersion.groovy
@@ -0,0 +1,36 @@
+def call(packageDir, filter) {
+    packageVersion = powershell returnStdout: true, script: '''$dir = "\\\\COAUTFSARH001\\Builds\\NugetPackages\\AT\\Platform"
+                    $filter="Reed.CommunicationIntegration.Service*"
+                    $latest = Get-ChildItem -Path $dir -Filter $filter | Sort-Object -Descending { [regex]::Replace($_, '\\d+', { $args[0].Value.PadLeft(20) })} | Select-Object -First 1
+                    $filterArray = $filter.Split(".")
+                    $fileNameArray = $latest.name.Split(".")
+                    $versionIndex = $filterArray.length + 2
+                    $oldVersion = [convert]::ToInt32($fileNameArray[$versionIndex],10)
+                    $newVersion = $oldVersion + 1
+                    $newVersion'''
+                    packageVersion = packageVersion.trim()
+                    return packageVersion
+}
+
+//pipeline {
+//    agent any
+//    stages {
+//        stage('Setting Versions') {
+//            steps {
+//                script {
+//                    packageVersion = powershell returnStdout: true, script: '''$dir = "\\\\COAUTFSARH001\\Builds\\NugetPackages\\AT\\Platform"
+//                    $filter="Reed.CommunicationIntegration.Service*"
+//                    $latest = Get-ChildItem -Path $dir -Filter $filter | Sort-Object -Descending { [regex]::Replace($_, '\\d+', { $args[0].Value.PadLeft(20) })} | Select-Object -First 1
+//                    $filterArray = $filter.Split(".")
+//                    $fileNameArray = $latest.name.Split(".")
+//                    $versionIndex = $filterArray.length + 2
+//                    $oldVersion = [convert]::ToInt32($fileNameArray[$versionIndex],10)
+//                    $newVersion = $oldVersion + 1
+//                    $newVersion'''
+//                    packageVersion = packageVersion.trim()
+//                    bat "echo ${packageVersion}" 
+//                }
+//            }
+//        }
+//    }
+//}
