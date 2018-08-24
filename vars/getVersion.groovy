5b16fd7812463fa4f80c45ef8afa419d0c4ebaed
Dan Nussbaumer <dnussbaumer@reedgroup.com>
2018-08-24 09:24:28 -0600

    update

--- vars/getVersion.groovy
+++ vars/getVersion.groovy
@@ -1,7 +1,6 @@
 def call(packageDir, filter) {
-    def psScript = "\$dir = '${packageDir}'"
-//                    Start-Sleep -s 15
-//                    $filter="${filter}"
+    def psScript = "\$dir = '${packageDir}'" +
+                    "\$filter='${filter}'"
 //                    $latest = Get-ChildItem -Path $dir -Filter $filter | Sort-Object -Descending { [regex]::Replace($_, '\\d+', { $args[0].Value.PadLeft(20) })} | Select-Object -First 1
 //                    $filterArray = $filter.Split(".")
 //                    $fileNameArray = $latest.name.Split(".")
