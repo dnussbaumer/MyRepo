091ff8f21f3472a0dee6b3a3d04cbd5857c815ba
Dan Nussbaumer <dnussbaumer@reedgroup.com>
2018-08-24 09:13:37 -0600

    update

--- vars/getVersion.groovy
+++ vars/getVersion.groovy
@@ -1,14 +1,14 @@
 def call(packageDir, filter) {
-    def psScript = '''$dir = "${packageDir}"\n\
-                    Start-Sleep -s 15
-                    $filter="${filter}"
-                    $latest = Get-ChildItem -Path $dir -Filter $filter | Sort-Object -Descending { [regex]::Replace($_, '\\d+', { $args[0].Value.PadLeft(20) })} | Select-Object -First 1
-                    $filterArray = $filter.Split(".")
-                    $fileNameArray = $latest.name.Split(".")
-                    $versionIndex = $filterArray.length + 2
-                    $oldVersion = [convert]::ToInt32($fileNameArray[$versionIndex],10)
-                    $newVersion = $oldVersion + 1
-                    $newVersion'''
+    def psScript = '''$dir = "${packageDir}"'''
+//                    Start-Sleep -s 15
+//                    $filter="${filter}"
+//                    $latest = Get-ChildItem -Path $dir -Filter $filter | Sort-Object -Descending { [regex]::Replace($_, '\\d+', { $args[0].Value.PadLeft(20) })} | Select-Object -First 1
+//                    $filterArray = $filter.Split(".")
+//                    $fileNameArray = $latest.name.Split(".")
+//                    $versionIndex = $filterArray.length + 2
+//                    $oldVersion = [convert]::ToInt32($fileNameArray[$versionIndex],10)
+//                    $newVersion = $oldVersion + 1
+//                    $newVersion'''
     bat "echo ${psScript}"
     def packageVersion = "11"
 //    def packageVersion = powershell returnStdout: true, script: '''$dir = "${packageDir}"\n\
