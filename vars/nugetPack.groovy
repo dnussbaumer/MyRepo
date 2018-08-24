c8619b7c7999732338b69397db51823d396927f2
Dan Nussbaumer <dnussbaumer@reedgroup.com>
2018-08-24 11:29:29 -0600

    update

--- vars/nugetPack.groovy
+++ vars/nugetPack.groovy
@@ -16,7 +16,7 @@ def call(project, configFilePath, packageLocation, version) {
     bat 'if exist artifacts rmdir artifacts /s /q'
     bat 'mkdir artifacts'
     for (i in 0..endingIndex) {
-        batCommand += "\"C:\\Program Files (x86)\\Nuget\\nuget.exe\" pack \"${nuspecLocations[i]}\" -Version ${version} -OutputDirectory artifacts/ -NoPackageAnalysis\n"
+        batCommand += "\"C:\\Program Files (x86)\\Nuget\\nuget.exe\" pack \"${nuspecLocations[i]}\" -Version \"${version}\" -OutputDirectory artifacts/ -NoPackageAnalysis\n"
     }
     batCommand += "\"C:\\Program Files (x86)\\Nuget\\nuget.exe\" push -source ${packageLocation} artifacts\\*.${version}.nupkg"
     bat batCommand
