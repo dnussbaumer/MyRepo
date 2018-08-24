37d800bd3b8f18c509467bf28663b436d8b93514
Dan Nussbaumer <dnussbaumer@reedgroup.com>
2018-08-24 09:05:51 -0600

    update

--- vars/getVersion.groovy
+++ vars/getVersion.groovy
@@ -1,5 +1,5 @@
 def call(packageDir, filter) {
-    def packageVersion = powershell returnStdout: true, script: '''$dir = "${packageDir}"\n\
+    def psScript = '''$dir = "${packageDir}"\n\
                     Start-Sleep -s 15
                     $filter="${filter}"
                     $latest = Get-ChildItem -Path $dir -Filter $filter | Sort-Object -Descending { [regex]::Replace($_, '\\d+', { $args[0].Value.PadLeft(20) })} | Select-Object -First 1
@@ -9,6 +9,18 @@ def call(packageDir, filter) {
                     $oldVersion = [convert]::ToInt32($fileNameArray[$versionIndex],10)
                     $newVersion = $oldVersion + 1
                     $newVersion'''
+    bat "echo ${psScript}"
+    def packageVersion = "11"
+//    def packageVersion = powershell returnStdout: true, script: '''$dir = "${packageDir}"\n\
+//                    Start-Sleep -s 15
+//                    $filter="${filter}"
+//                    $latest = Get-ChildItem -Path $dir -Filter $filter | Sort-Object -Descending { [regex]::Replace($_, '\\d+', { $args[0].Value.PadLeft(20) })} | Select-Object -First 1
+//                    $filterArray = $filter.Split(".")
+//                    $fileNameArray = $latest.name.Split(".")
+//                    $versionIndex = $filterArray.length + 2
+//                    $oldVersion = [convert]::ToInt32($fileNameArray[$versionIndex],10)
+//                    $newVersion = $oldVersion + 1
+//                    $newVersion'''
     packageVersion = packageVersion.trim()
     return packageVersion
 }
