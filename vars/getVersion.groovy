c3a8d963ad93313e8bd9cc908f1c3fdfb9f38d8d
Dan Nussbaumer <dnussbaumer@reedgroup.com>
2018-08-24 09:59:35 -0600

    update

--- vars/getVersion.groovy
+++ vars/getVersion.groovy
@@ -1,5 +1,4 @@
 def call(packageDir, filter) {
-    println "Hello"
     def psScript = """\$dir = '${packageDir}'
                     \$filter='${filter}'
                     \$latest = Get-ChildItem -Path \$dir -Filter \$filter | Sort-Object -Descending { [regex]::Replace(\$_, '\\d+', { \$args[0].Value.PadLeft(20) })} | Select-Object -First 1
@@ -9,41 +8,7 @@ def call(packageDir, filter) {
                     \$oldVersion = [convert]::ToInt32(\$fileNameArray[\$versionIndex],10)
                     \$newVersion = \$oldVersion + 1
                     \$newVersion"""
-    println psScript
-//    def packageVersion = "11"
     def packageVersion = powershell returnStdout: true, script: psScript
-//                    Start-Sleep -s 15
-//                    $filter="${filter}"
-//                    $latest = Get-ChildItem -Path $dir -Filter $filter | Sort-Object -Descending { [regex]::Replace($_, '\\d+', { $args[0].Value.PadLeft(20) })} | Select-Object -First 1
-//                    $filterArray = $filter.Split(".")
-//                    $fileNameArray = $latest.name.Split(".")
-//                    $versionIndex = $filterArray.length + 2
-//                    $oldVersion = [convert]::ToInt32($fileNameArray[$versionIndex],10)
-//                    $newVersion = $oldVersion + 1
-//                    $newVersion'''
     packageVersion = packageVersion.trim()
     return packageVersion
 }
-
-//pipeline {
-//    agent any
-//    stages {
-//        stage('Setting Versions') {
-//            steps {
-//                script {
-//                    packageVersion = powershell returnStdout: true, script: '''$dir = "\\\\COAUTFSARH001\\Builds\\NugetPackages\\AT\\Platform"
-//                    $filter="Reed.CommunicationIntegration.Service*"
-//                    $latest = Get-ChildItem -Path $dir -Filter $filter | Sort-Object -Descending { [regex]::Replace($_, '\\d+', { $args[0].Value.PadLeft(20) })} | Select-Object -First 1
-//                    $filterArray = $filter.Split(".")
-//                    $fileNameArray = $latest.name.Split(".")
-//                    $versionIndex = $filterArray.length + 2
-//                    $oldVersion = [convert]::ToInt32($fileNameArray[$versionIndex],10)
-//                    $newVersion = $oldVersion + 1
-//                    $newVersion'''
-//                    packageVersion = packageVersion.trim()
-//                    bat "echo ${packageVersion}" 
-//                }
-//            }
-//        }
-//    }
-//}
