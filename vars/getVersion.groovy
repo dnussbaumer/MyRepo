edc45c88c69695b8bbb6348066fe31a2b4651b4c
Dan Nussbaumer <dnussbaumer@reedgroup.com>
2018-08-24 09:46:23 -0600

    update

--- vars/getVersion.groovy
+++ vars/getVersion.groovy
@@ -2,8 +2,8 @@ def call(packageDir, filter) {
     println "Hello"
     def psScript = """\$dir = '${packageDir}'
                     \$filter='${filter}'
-                    \$latest = Get-ChildItem -Path \$dir -Filter \$filter | Sort-Object -Descending { [regex]::Replace(\$_, '\\d+', { \$args[0].Value.PadLeft(20) })} | Select-Object -First 1\n"""
-//                    \$filterArray = \$filter.Split('.')\n
+                    \$latest = Get-ChildItem -Path \$dir -Filter \$filter | Sort-Object -Descending { [regex]::Replace(\$_, '\\d+', { \$args[0].Value.PadLeft(20) })} | Select-Object -First 1
+                    \$filterArray = \$filter.Split('.')"""
 //                    \$fileNameArray = \$latest.name.Split('.')\n
 //                    \$versionIndex = \$filterArray.length + 2\n
 //                    \$oldVersion = [convert]::ToInt32(\$fileNameArray[\$versionIndex],10)\n
