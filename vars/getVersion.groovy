eec18642b722c33632e2e45737548e3064a8e609
Dan Nussbaumer <dnussbaumer@reedgroup.com>
2018-08-24 09:47:38 -0600

    update

--- vars/getVersion.groovy
+++ vars/getVersion.groovy
@@ -3,12 +3,12 @@ def call(packageDir, filter) {
     def psScript = """\$dir = '${packageDir}'
                     \$filter='${filter}'
                     \$latest = Get-ChildItem -Path \$dir -Filter \$filter | Sort-Object -Descending { [regex]::Replace(\$_, '\\d+', { \$args[0].Value.PadLeft(20) })} | Select-Object -First 1
-                    \$filterArray = \$filter.Split('.')"""
-//                    \$fileNameArray = \$latest.name.Split('.')\n
-//                    \$versionIndex = \$filterArray.length + 2\n
-//                    \$oldVersion = [convert]::ToInt32(\$fileNameArray[\$versionIndex],10)\n
-//                    \$newVersion = \$oldVersion + 1\n
-//                    \$newVersion"""
+                    \$filterArray = \$filter.Split('.')
+                    \$fileNameArray = \$latest.name.Split('.')
+                    \$versionIndex = \$filterArray.length + 2
+                    \$oldVersion = [convert]::ToInt32(\$fileNameArray[\$versionIndex],10)
+                    \$newVersion = \$oldVersion + 1
+                    \$newVersion"""
     println psScript
     def packageVersion = "11"
 //    def packageVersion = powershell returnStdout: true, script: '''$dir = "${packageDir}"\n\
