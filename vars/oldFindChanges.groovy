04090e6bb489d41429b4d0387733666feb051f33
Dan Nussbaumer <dnussbaumer@reedgroup.com>
2018-09-11 13:22:15 -0600

    update

---
+++ vars/oldFindChanges.groovy
@@ -0,0 +1,19 @@
+def call(List configList, file) {
+    def endingOuterIndex = configList.size() - 1
+    def endingInnerIndex = 0
+    def changeFound = 'false'
+    def i = 0
+    while (i <= endingOuterIndex && changeFound == 'false') {
+        changeFound = configList[i][1]
+        if (changeFound == 'false') {
+            endingInnerIndex = configList[i][2].size() -1
+            for (j in 0..endingInnerIndex) {
+                if (file.path.contains(configList[i][2][j])) {
+                    changeFound = 'true'
+                }
+            }
+        }
+        i++
+    }
+    return changeFound
+}
